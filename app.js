842cee3d43feafda2e26249d49d2d8a83040b6b6
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-19 18:01:00 +0500

    commit-tree

--- app.js
+++ app.js
@@ -21,5 +21,21 @@
 // var result=(zakatPercentage*userInput);
 // alert("Your Zakat Value Is "+result)
 
-var name="mark";
-alert=(name)
+// var name="mark";
+// alert=(name)
+
+// TASK 3:
+// Create a program that generates a random number between 1 and 10 and stores it
+// in a variable called "secretNumber". Then, ask the user to enter a guess for the
+// secret number using a prompt.Use an if-else statement to check if the user's guess matches the secret number. Ifthe guess is correct, display a message using an alert saying "Congratulations! You guessed the secret number". Otherwise, if the guess is too high or too low, display an appropriate message informing the user to guess again ?
+
+// ________TASK 3:___________________________________Answer__________________________________
+var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+var guess = prompt("Guess the secret number (between 1 and 10)");
+
+if (guess == secretNumber) {
+  alert("Congratulations! You guessed the secret number");
+} else {
+  alert("Sorry, the secret number was " + secretNumber + ". Better luck next time!");
+}
