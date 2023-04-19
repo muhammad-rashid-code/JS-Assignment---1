b0c839b2a4cfd7fbc3cd9963debc13dd53b635a6
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-19 17:22:11 +0500

    make style sheet

---
+++ app.js
@@ -0,0 +1,25 @@
+// ____________rabbi zidni ilma__________________
+// day 1
+// 4/19/2023
+// JS Assignment - 1
+// Deadline : 21 Apr , 2023 11:59pm
+
+// Task 1:
+// Calculate the zakat value, first, create a variable named "zakatPercentage" and store the value of 2.5% in it (hint: 2.5% means 0.025). Next, create another variable named "userInput" and take the input from the user using the prompt. Make sure the input value is of a type number by converting the input string to a number using a suitable method. Then, create a variable named "result" and assign its value to the multiplication of the zakat percentage and user input. Finally, use an alert message to display the calculated zakat value. The message should look like this: "Your zakat value is xxx".
+
+// var zakatPercentage=(0.025);
+// var userInput=+prompt("Enter Your Assets");
+// console.log(zakatPercentage+userInput);
+// ______________________________wrong
+
+// var date=4/19/2023
+// alert=(date)
+
+// __Task 1________Answer______________
+// var zakatPercentage=(0.025);
+// var userInput=+prompt("Enter Your Assets");
+// var result=(zakatPercentage*userInput);
+// alert("Your Zakat Value Is "+result)
+
+var name="mark";
+alert=(name)
