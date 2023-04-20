1790606234f61f803e0e60f615296433d554dfeb
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-04-20 16:55:28 +0500

    commit tree

--- app.js
+++ app.js
@@ -154,3 +154,16 @@ if (nationality == 1) {
 } else {
   alert("You are not eligible to vote.");
 }
+
+// Task 8:
+// You have an array of that contains the name of  Pakistani Teams Player selected for WorldCup (15 Players) named as WorldCupSquad. Now tomorrow we have a match with India, So make an array of final team players (11 Players) that will be playing in tomorrows match from the WorldCupSquad array. 
+// (Hint : Make sure it should not disturb the array that contains 15 players instead you have to make a copy of this array)
+
+// ___________Task 8:____________________Answer_______________________________
+const WorldCupSquad = ['Babar Azam', 'Fakhar Zaman', 'Imam-ul-Haq', 'Asif Ali', 'Haris Sohail', 'Mohammad Hafeez', 'Shadab Khan', 'Imad Wasim', 'Sarfaraz Ahmed (c&wk)', 'Mohammad Amir', 'Hasan Ali', 'Shaheen Afridi', 'Wahab Riaz', 'Junaid Khan', 'Shoaib Malik'];
+
+const PakistanPlaying11 = WorldCupSquad.slice(0, 11);
+
+alert('Pakistani World Cup Squad: ' + WorldCupSquad.join(', '));
+
+alert('Pakistani Playing 11 against India: ' + PakistanPlaying11.join(', '));
